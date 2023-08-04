6ad324d568d2082ac56fed7772b088f82d9ee8f8
luisfarias1 <141429017+luisfarias1@users.noreply.github.com>
2023-08-04 10:49:26 -0400

    primer comit

---
+++ header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> UTC</title>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous">
+    </script>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </head>
+  <body>
+
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand"
+          href="<?php echo site_url(); ?>/welcome"
+           href="">LUIS FARIAS</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        
+          <form class="navbar-form navbar-left">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">-</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">CONTACTO <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">CORREO</a></li>
+                <li><a href="#">TELEFONO</a></li>
+                <li><a href="#">FACEBOOK</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#"></a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
---
+++ welcome_message.php
@@ -0,0 +1,112 @@
+<div class="well text-center">
+	<h4>
+		<i class="glyphicon glyphicon-home"></i>
+   <b>UTC </b>
+	</h4>
+</div>
+
+<!-- carrusel de imagenes -->
+<main>
+
+<hr>
+<title>UTC</title>
+	 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	 <link rel="Stylesheet" href="common.css" />
+
+<head>
+				<center>
+			 <h1>PORQUE ESCOJISTE LA CARRERA?</h1>
+			 <p>LA informatica es algo que siempre me ah llamado la atencion
+         es decir la tecnologia y como avanza la ciencia cada dia mas
+         en el colegio donde estudie comence a apacionarme mas por la inormfatica
+         me gusta el ambito de redes o infraestructura de redes de igual manera me
+         llamaba la atencion
+			 </p>
+					</p>
+					<br>
+					<br>
+					<center>
+			 <h2>ES BUENA LA CARRERA QUE ESCOJI</h2>
+			 <p>De igual manera en mi familia tambien les gusta la tecnologia
+       es decir lo nuevo que surge en el mundo de la computadora
+       con el pasar del tiempo podemos darnos cuenta de que todo lo que nos rodea
+     es informatica y comunicacion entre usuarios porque esta  todo conectado al intenet</p>
+		 </center>
+	 </main>
+   <center>
+  <h1>LA INFORMATICA?</h1>
+  <p>LLa Informática es la rama de la Ingeniería que estudia el hardware, las redes de datos
+     y el software necesarios para tratar información de forma automática. Aunque pueda
+     parecerte una definición muy abstracta, estamos seguros de que sabes mucho más de
+     Informática de lo que crees. Y si no, sigue leyendo un poco más.
+     Seguro que te suena qué es el hardware. Y si no te suena, seguro que has utilizado
+     hardware en muchas ocasiones sin saber que se llama así.  El hardware son los ordenadores
+     de sobremesa, los portátiles, los tablets, los teléfonos móviles, las impresoras
+     las consolas de videojuegos, los lectores de DVDs, los reproductores de música, etcétera.
+      ¿A que sí que sabías qué es el hardware? Lo que quizá no sabías es que estos aparatos están
+      formados internamente por componentes electrónicos a los que también se les llama hardware:
+  </p>
+     </p>
+     <br>
+     <br>
+     <center>
+       <center>
+      <h1>NOMBRE </h1>
+      <p>HOLA  MI NOMBRE ES LUIS FARIAS
+        SOY ESTUDIANTE DE LA CARRERA DE LA INFORMACION
+      </p>
+         </p>
+         <br>
+         <br>
+         <center>
+	 <style>
+
+
+
+
+
+
+			 /* Para el ancho y alto de las imagenes. Modificalo a gusto =)  */
+			 .carousel-inner > .item > img,
+			 .carousel-inner > .item > a > img {
+					 width: 100%;
+					 height: 500px;
+					 margin: auto;
+			 }
+			 main {
+							column-count:2;
+					}
+			 /* Para los margenes del contendor de las fotos. Podes cambiarle el ancho a gusto tambien! */
+			 .carousel-control {
+					 width: 10%;
+			 }
+	 </style>
+	</head>
+
+
+	<div class="container">
+	<br>
+
+	 <!-- Opcional, bullets abajo para saltar directamente a una foto (si los pones tiene q haber 1 por foto) -->
+	 <
+
+	 <!-- Contenedor de las fotos -->
+
+		 <!-- Foto 3 -->
+
+
+	 </div>
+	 <!-- Controles para pasar las fotos -->
+	 <a class="left carousel-control" href="#carrusel" role="button" data-slide="prev">
+		 <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		 <span class="sr-only">Previous</span>
+	 </a>
+	 <a class="right carousel-control" href="#carrusel" role="button" data-slide="next">
+		 <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		 <span class="sr-only">Next</span>
+	 </a>
+	  </div>
+	</div>
+
+
+</div>
